2a796d0877659bae076157ca11161aae6a023387
ayelet <ayelet2708@gmail.com>
2022-07-28 13:40:17 +0300

    edit: components exp inc

--- src/Components/NewAddition/NewAddition.jsx
+++ src/Components/NewAddition/NewAddition.jsx
@@ -1,9 +1,9 @@
 import "./NewAddition.css";
 import {
-  DialogContentContainer,
-  Heading,
-  Button,
-  TextField,
+	DialogContentContainer,
+	Heading,
+	Button,
+	TextField,
 } from "monday-ui-react-core";
 import { Description } from "monday-ui-react-core/dist/allIcons";
 import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
@@ -22,154 +22,154 @@ import { useState, useCallback } from "react";
 import ErrorToast from "../toasts/ErrorToast";
 
 const radioButtons = [
-  { icon: HomeOutlinedIcon, name: CATEGORIES.RENT },
-  { icon: FastfoodOutlinedIcon, name: CATEGORIES.FOOD },
-  { icon: DirectionsCarFilledOutlinedIcon, name: CATEGORIES.CAR },
-  { icon: ConstructionOutlinedIcon, name: CATEGORIES.CONSTRUCTION },
-  { icon: PetsOutlinedIcon, name: CATEGORIES.PETS },
-  { icon: SchoolOutlinedIcon, name: CATEGORIES.SCHOOL },
-  { icon: ShoppingBagOutlinedIcon, name: CATEGORIES.SHOPPING },
-  { icon: LuggageOutlinedIcon, name: CATEGORIES.VACATION },
-  { icon: MoreHorizIcon, name: CATEGORIES.OTHER },
+	{ icon: HomeOutlinedIcon, name: CATEGORIES.RENT },
+	{ icon: FastfoodOutlinedIcon, name: CATEGORIES.FOOD },
+	{ icon: DirectionsCarFilledOutlinedIcon, name: CATEGORIES.CAR },
+	{ icon: ConstructionOutlinedIcon, name: CATEGORIES.CONSTRUCTION },
+	{ icon: PetsOutlinedIcon, name: CATEGORIES.PETS },
+	{ icon: SchoolOutlinedIcon, name: CATEGORIES.SCHOOL },
+	{ icon: ShoppingBagOutlinedIcon, name: CATEGORIES.SHOPPING },
+	{ icon: LuggageOutlinedIcon, name: CATEGORIES.VACATION },
+	{ icon: MoreHorizIcon, name: CATEGORIES.OTHER },
 ];
 
 function NewAddition({ addType, fixed, modalVisible, setModalVisible }) {
-  const [submition, setSubmition] = useState({
-    description: null,
-    value: null,
-    category: null,
-    startDate: null,
-    endDate: null,
-  });
-  const [loadingSubmit, setLoadingSubmit] = useState(false);
-  const [openToast, setOpenToast] = useState(false);
-  const openToastCallback = useCallback(() => {
-    setLoadingSubmit(false);
-    setOpenToast((openToast) => !openToast);
-  }, [setOpenToast]);
+	const [submition, setSubmition] = useState({
+		description: null,
+		value: null,
+		category: null,
+		startDate: null,
+		endDate: null,
+	});
+	const [loadingSubmit, setLoadingSubmit] = useState(false);
+	const [openToast, setOpenToast] = useState(false);
+	const openToastCallback = useCallback(() => {
+		setLoadingSubmit(false);
+		setOpenToast((openToast) => !openToast);
+	}, [setOpenToast]);
 
-  const handleSubmit = async () => {
-    setLoadingSubmit(true);
-    for (const key in submition) {
-      if (fixed) {
-        if (!submition[key]) {
-          openToastCallback();
-          return;
-        }
-      } else {
-        if (key !== "startDate" && key !== "endDate" && !submition[key]) {
-          openToastCallback();
-          return;
-        }
-      }
-    }
+	const handleSubmit = async () => {
+		setLoadingSubmit(true);
+		for (const key in submition) {
+			if (fixed) {
+				if (!submition[key]) {
+					openToastCallback();
+					return;
+				}
+			} else {
+				if (key !== "startDate" && key !== "endDate" && !submition[key]) {
+					openToastCallback();
+					return;
+				}
+			}
+		}
 
-    // submit to DB
+		// submit to DB
 
-    setLoadingSubmit(false);
-    Object.keys(submition).forEach((key) => (submition[key] = null));
-    setModalVisible(false);
-  };
+		setLoadingSubmit(false);
+		Object.keys(submition).forEach((key) => (submition[key] = null));
+		setModalVisible(false);
+	};
 
-  return modalVisible ? (
-    <>
-      <ErrorToast isVisible={openToast} message="Invalid Input!" />
-      <div className="darkBG" onClick={() => setModalVisible(false)} />
-      <div className="modal">
-        <DialogContentContainer
-          className="modalContent"
-          size={DialogContentContainer.sizes.LARGE}
-        >
-          <Heading
-            value={`Add your new ${addType}`}
-            className="modalTitle"
-            type={Heading.types.h3}
-          />
-          <div className="inputs">
-            <TextField
-              placeholder="Enter Description"
-              size={TextField.sizes.MEDIUM}
-              iconName={Description}
-              onChange={(value) =>
-                setSubmition({ ...submition, description: value })
-              }
-              loading={loadingSubmit}
-            />
-            <TextField
-              placeholder="0.00"
-              type="number"
-              size={TextField.sizes.MEDIUM}
-              iconName={AttachMoneyIcon}
-              loading={loadingSubmit}
-              onChange={(value) => setSubmition({ ...submition, value: value })}
-            />
-          </div>
+	return modalVisible ? (
+		<>
+			<ErrorToast isVisible={openToast} message="Invalid Input!" />
+			<div className="darkBG" onClick={() => setModalVisible(false)} />
+			<div className="modal">
+				<DialogContentContainer
+					className="modalContent"
+					size={DialogContentContainer.sizes.LARGE}
+				>
+					<Heading
+						value={`Add your new ${addType}`}
+						className="modalTitle"
+						type={Heading.types.h3}
+					/>
+					<div className="inputs">
+						<TextField
+							placeholder="Enter Description"
+							size={TextField.sizes.MEDIUM}
+							iconName={Description}
+							onChange={(value) =>
+								setSubmition({ ...submition, description: value })
+							}
+							loading={loadingSubmit}
+						/>
+						<TextField
+							placeholder="0.00"
+							type="number"
+							size={TextField.sizes.MEDIUM}
+							iconName={AttachMoneyIcon}
+							loading={loadingSubmit}
+							onChange={(value) => setSubmition({ ...submition, value: value })}
+						/>
+					</div>
 
-          {fixed ? (
-            <div className="fixedDates">
-              From
-              <TextField
-                size={TextField.sizes.SMALL}
-                type={TextField.types.DATE}
-                onChange={(value) =>
-                  setSubmition({ ...submition, startDate: value })
-                }
-              />
-              To
-              <TextField
-                size={TextField.sizes.SMALL}
-                type={TextField.types.DATE}
-                onChange={(value) =>
-                  setSubmition({ ...submition, endDate: value })
-                }
-              />
-            </div>
-          ) : (
-            <></>
-          )}
+					{fixed ? (
+						<div className="fixedDates">
+							From
+							<TextField
+								size={TextField.sizes.SMALL}
+								type={TextField.types.DATE}
+								onChange={(value) =>
+									setSubmition({ ...submition, startDate: value })
+								}
+							/>
+							To
+							<TextField
+								size={TextField.sizes.SMALL}
+								type={TextField.types.DATE}
+								onChange={(value) =>
+									setSubmition({ ...submition, endDate: value })
+								}
+							/>
+						</div>
+					) : (
+						<></>
+					)}
 
-          {addType === "Expenses" ? (
-            <>
-              <Heading
-                value="Pick the Category:"
-                className="modalTitle"
-                type={Heading.types.h4}
-              />
-              <div className="typeSelection">
-                {radioButtons.map((radioBtn) => (
-                  <RadioButtonWrapper
-                    icon={radioBtn.icon}
-                    name={radioBtn.name}
-                    key={radioBtn.name}
-                    onClick={() =>
-                      setSubmition({ ...submition, category: radioBtn.name })
-                    }
-                  />
-                ))}
-              </div>{" "}
-            </>
-          ) : (
-            <></>
-          )}
+					{addType === "Expenses" ? (
+						<>
+							<Heading
+								value="Pick the Category:"
+								className="modalTitle"
+								type={Heading.types.h4}
+							/>
+							<div className="typeSelection">
+								{radioButtons.map((radioBtn) => (
+									<RadioButtonWrapper
+										icon={radioBtn.icon}
+										name={radioBtn.name}
+										key={radioBtn.name}
+										onClick={() =>
+											setSubmition({ ...submition, category: radioBtn.name })
+										}
+									/>
+								))}
+							</div>{" "}
+						</>
+					) : (
+						<></>
+					)}
 
-          <div className="modalButtons">
-            <Button className="modalActionBtn" onClick={() => handleSubmit()}>
-              Submit
-            </Button>
-            <Button
-              className="modalActionBtn"
-              onClick={() => setModalVisible(false)}
-              kind={Button.kinds.TERTIARY}
-            >
-              Cancel
-            </Button>
-          </div>
-        </DialogContentContainer>
-      </div>
-    </>
-  ) : (
-    <></>
-  );
+					<div className="modalButtons">
+						<Button className="modalActionBtn" onClick={() => handleSubmit()}>
+							Submit
+						</Button>
+						<Button
+							className="modalActionBtn"
+							onClick={() => setModalVisible(false)}
+							kind={Button.kinds.TERTIARY}
+						>
+							Cancel
+						</Button>
+					</div>
+				</DialogContentContainer>
+			</div>
+		</>
+	) : (
+		<></>
+	);
 }
 
 export default NewAddition;
--- src/Components/compareExpensesIncomes/CompareExpensesIncome.jsx
+++ src/Components/compareExpensesIncomes/CompareExpensesIncome.jsx
@@ -1,49 +1,80 @@
-import React from "react";
+import { React, useEffect, useMemo } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+	BarChart,
+	Bar,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	Legend,
+} from "recharts";
+import { Heading } from "monday-ui-react-core";
+
+import { fetchAllFixedEvents } from "../../redux/fixedEvents/actions/fetchAllFixedEvents";
+
 import styles from "./compareExpenesIncome.module.scss";
 
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from "recharts";
-
-const data = [
-  {
-    name: "Month",
-    Expenses: 4000,
-    Incomes: 2400,
-    amt: 1000,
-  },
-];
-
 const CompareExpenesIncomes = () => {
-  return (
-    <div className={styles.container}>
-      <BarChart
-        width={300}
-        height={300}
-        data={data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="Expenses" fill="#DC143C" />
-        <Bar dataKey="Incomes" fill="#82ca9d" />
-      </BarChart>
-    </div>
-  );
+	const dispatch = useDispatch();
+
+	const fixedEvents = useSelector(
+		(state) => state.fixedEventsState.fixedEvents
+	);
+
+	const calculateSumOfFixedEvents = (fixedEvents) => {
+		let sumOfExpenses = 0;
+		let sumOfIncomes = 0;
+		fixedEvents.forEach(({ type, value }) => {
+			if (type === "expense") {
+				sumOfExpenses += value;
+			} else {
+				sumOfIncomes += value;
+			}
+		});
+		return [sumOfExpenses, sumOfIncomes];
+	};
+
+	const [sumOfExpenses, sumOfIncomes] = useMemo(
+		() => calculateSumOfFixedEvents(fixedEvents),
+		[fixedEvents]
+	);
+
+	useEffect(() => {
+		dispatch(fetchAllFixedEvents("1"));
+	}, [dispatch]);
+
+	return (
+		<div className={styles.container}>
+			<Heading value="Expenses vs. Incomes" type={Heading.types.h2} />
+			<BarChart
+				width={300}
+				height={300}
+				data={[
+					{
+						name: "Month",
+						Expenses: sumOfExpenses,
+						Incomes: sumOfIncomes,
+						amt: 1000,
+					},
+				]}
+				margin={{
+					top: 5,
+					right: 30,
+					left: 20,
+					bottom: 5,
+				}}
+			>
+				<CartesianGrid strokeDasharray="3 3" />
+				<XAxis dataKey="name" />
+				<YAxis />
+				<Tooltip />
+				<Legend />
+				<Bar dataKey="Expenses" fill="#DC143C" />
+				<Bar dataKey="Incomes" fill="#82ca9d" />
+			</BarChart>
+		</div>
+	);
 };
 
 export default CompareExpenesIncomes;
