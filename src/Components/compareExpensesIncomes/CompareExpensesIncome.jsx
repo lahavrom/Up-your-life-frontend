06b785c912ea07963c5e0ab547d06f2e8c2202d4
Lahav Rom <lahavrom@gmail.com>
2022-07-29 11:03:41 +0300

    fix: after rebase

--- src/Components/compareExpensesIncomes/CompareExpensesIncome.jsx
+++ src/Components/compareExpensesIncomes/CompareExpensesIncome.jsx
@@ -8,74 +8,76 @@ import {
   Legend,
 } from "recharts";
 import { Heading } from "monday-ui-react-core";
-
+import { useDispatch, useSelector } from "react-redux";
+import { useMemo, useEffect } from "react";
 import styles from "./compareExpenesIncome.module.css";
+import { fetchAllFixedEvents } from "../../redux/fixedEvents/actions/fetchAllFixedEvents";
 
 const CompareExpenesIncomes = () => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const fixedEvents = useSelector(
-		(state) => state.fixedEventsState.fixedEvents
-	);
+  const fixedEvents = useSelector(
+    (state) => state.fixedEventsState.fixedEvents
+  );
 
-	const calculateSumOfFixedEvents = (fixedEvents) => {
-		let sumOfExpenses = 0;
-		let sumOfIncomes = 0;
-		fixedEvents.forEach(({ type, value }) => {
-			if (type === "expense") {
-				sumOfExpenses += value;
-			} else {
-				sumOfIncomes += value;
-			}
-		});
-		return [sumOfExpenses, sumOfIncomes];
-	};
+  const calculateSumOfFixedEvents = (fixedEvents) => {
+    let sumOfExpenses = 0;
+    let sumOfIncomes = 0;
+    fixedEvents.forEach(({ type, value }) => {
+      if (type === "expense") {
+        sumOfExpenses += value;
+      } else {
+        sumOfIncomes += value;
+      }
+    });
+    return [sumOfExpenses, sumOfIncomes];
+  };
 
-	const [sumOfExpenses, sumOfIncomes] = useMemo(
-		() => calculateSumOfFixedEvents(fixedEvents),
-		[fixedEvents]
-	);
+  const [sumOfExpenses, sumOfIncomes] = useMemo(
+    () => calculateSumOfFixedEvents(fixedEvents),
+    [fixedEvents]
+  );
 
-	useEffect(() => {
-		dispatch(fetchAllFixedEvents());
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchAllFixedEvents());
+  }, [dispatch]);
 
-	return (
-		<div className={styles.container}>
-			<Heading value="Expenses vs. Incomes" type={Heading.types.h2} />
-			<Heading
-				type={Heading.types.h2}
-				value="This month comparing your expenses vs. your Incomes:"
-				size="small"
-			/>
-			<BarChart
-				width={300}
-				height={300}
-				data={[
-					{
-						name: "Month",
-						Expenses: sumOfExpenses,
-						Incomes: sumOfIncomes,
-						amt: 1000,
-					},
-				]}
-				margin={{
-					top: 5,
-					right: 30,
-					left: 20,
-					bottom: 5,
-				}}
-			>
-				<CartesianGrid strokeDasharray="3 3" />
-				<XAxis dataKey="name" />
-				<YAxis />
-				<Tooltip />
-				<Legend />
-				<Bar dataKey="Expenses" fill="#D57E7E" />
-				<Bar dataKey="Incomes" fill="#82ca9d" />
-			</BarChart>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <Heading value="Expenses vs. Incomes" type={Heading.types.h2} />
+      <Heading
+        type={Heading.types.h2}
+        value="This month comparing your expenses vs. your Incomes:"
+        size="small"
+      />
+      <BarChart
+        width={300}
+        height={300}
+        data={[
+          {
+            name: "Month",
+            Expenses: sumOfExpenses,
+            Incomes: sumOfIncomes,
+            amt: 1000,
+          },
+        ]}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="Expenses" fill="#D57E7E" />
+        <Bar dataKey="Incomes" fill="#82ca9d" />
+      </BarChart>
+    </div>
+  );
 };
 
 export default CompareExpenesIncomes;
--- src/pages/upYourLife/UpYourLifePage.jsx
+++ src/pages/upYourLife/UpYourLifePage.jsx
@@ -1,13 +1,10 @@
-import Sidebar from "../../components/sidebar/Sidebar";
+// import Sidebar from "../../components/sidebar/Sidebar";
 import Dashboard from "../../components/Dashboard";
 import styles from "./upYourLife.module.css";
 
 const UpYourLifePage = () => {
   return (
     <div className={styles.container}>
-      {/* <div className={styles.sidebarContainer}>
-        <Sidebar />
-      </div> */}
       <div className={styles.contentContainer}>
         <div id="dashboard">
           <Dashboard />
