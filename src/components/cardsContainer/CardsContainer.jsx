158a7486631bb4d9a9335bd2d83cb5ee7bf2e662
Lahav Rom <lahavrom@gmail.com>
2022-07-31 16:36:56 +0300

    deleted components folder

--- src/components/cardsContainer/CardsContainer.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./cardsContainer.module.css";
-
-const CardsContainer = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
-};
-
-export default CardsContainer;
