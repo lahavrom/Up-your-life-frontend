1d8e84afd500abf5c645502b7b970f73ad0ade49
Lahav Rom <lahavrom@gmail.com>
2022-08-02 21:14:41 +0300

    feat: upload image to S3 bucket in progress

--- src/components/transactionLog/components/transactionAvatar/TransactionAvatar.jsx
+++ src/components/transactionLog/components/transactionAvatar/TransactionAvatar.jsx
@@ -1,4 +1,5 @@
-// import styles from "./transactionAvatar.module.css";
+import { useState, useRef } from "react";
+import axios from "axios";
 import { Avatar } from "monday-ui-react-core";
 
 const TransactionAvatar = (userId) => {
@@ -8,18 +9,52 @@ const TransactionAvatar = (userId) => {
   };
 
   const { userName, userText, userPhoto } = getUserData(userId);
-  return !userPhoto ? (
-    <Avatar
-      size={Avatar.sizes.MEDIUM}
-      type={Avatar.types.TEXT}
-      text={userText}
-      backgroundColor={Avatar.colors.DARK_BLUE}
-      ariaLabel={userName}
-    />
+  const inputImage = useRef(null);
+  const [newUserPhoto, setNewUserPhoto] = useState(userPhoto);
+  const [photo, setPhoto] = useState(userPhoto);
+
+  const convertToBase64 = (file) => {
+    return new Promise((resolve) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+    });
+  };
+
+  const uploadImage = async () => {
+    setNewUserPhoto(URL.createObjectURL(inputImage.current.files[0]));
+    setPhoto(true);
+    const convertedFile = await convertToBase64(newUserPhoto);
+
+    await axios.post("localhost:3001/users/upload-image", {
+      image: convertedFile,
+      imageName: userId,
+    });
+  };
+
+  return !photo ? (
+    <>
+      <input
+        type="file"
+        ref={inputImage}
+        style={{ display: "none" }}
+        onChange={() => uploadImage()}
+      />
+      <Avatar
+        onClick={() => inputImage.current.click()}
+        size={Avatar.sizes.MEDIUM}
+        type={Avatar.types.TEXT}
+        text={userText}
+        backgroundColor={Avatar.colors.DARK_BLUE}
+        ariaLabel={userName}
+      />
+    </>
   ) : (
     <Avatar
       size={Avatar.sizes.MEDIUM}
-      src={""}
+      src={newUserPhoto}
       type={Avatar.types.IMG}
       ariaLabel={userName}
     />
