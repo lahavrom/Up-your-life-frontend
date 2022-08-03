b2bfe4eaf3983a1eca38b3eb455c679e362f0090
Lahav Rom <lahavrom@gmail.com>
2022-08-03 15:10:42 +0300

    feat: selectors for arranging

--- src/components/transactionLog/TransactionLog.jsx
+++ src/components/transactionLog/TransactionLog.jsx
@@ -1,12 +1,13 @@
 import { useState, useMemo, useEffect } from "react";
 import { useSelector } from "react-redux";
 
+import { currentMonthAccountTransactions } from "../../redux/transactions/selectors/selectAccountTransactions";
+import { futureFixedTransactions } from "../../redux/transactions/selectors/selectFixedTransactions";
 import { Heading } from "monday-ui-react-core";
 import magnifyingGlass from "../../assets/magnifying-glass.png";
 import styles from "./transactionLog.module.css";
 import {
   makeDateTimestamp,
-  getMonth,
   makeDateFromDay,
 } from "../../helpers/dateTimeUtils";
 import CardsContainer from "../cardsContainer/CardsContainer";
@@ -14,18 +15,23 @@ import Card from "../card/Card";
 import TransactionLogHeader from "./components/transactionLogHeader/TransactionLogHeader";
 import TableTransactions from "./components/tableTransactions/TableTransactions";
 
-const filterFutureTransactions = (transactions) => {
-  return transactions.filter((elem) => elem.dayOfMonth > new Date().getDate());
-};
-
 const TransactionLog = ({ onEditTransaction }) => {
   const selectedMonth = useSelector(({ dateState }) => dateState.month);
 
-  const fixed = useSelector(({ transactionsState }) => transactionsState.fixed);
-
-  const account = useSelector(
-    ({ transactionsState }) => transactionsState.account
+  const fixed = useSelector(futureFixedTransactions);
+  const futureTransactions = useMemo(
+    () => [
+      ...fixed.map((elem) => {
+        return {
+          ...elem,
+          date: makeDateFromDay(elem.dayOfMonth, selectedMonth + 1),
+        };
+      }),
+    ],
+    [fixed, selectedMonth]
   );
+
+  const account = useSelector(currentMonthAccountTransactions);
 
   const [filteredTransactions, setFilteredTransactions] = useState([]);
   const [filteredFutureTransactions, setFilteredFutureTransactions] = useState(
@@ -33,44 +39,24 @@ const TransactionLog = ({ onEditTransaction }) => {
   );
   const [showFutureTransactions, setShowFutureTransactions] = useState(false);
 
-  const futureTransactions = useMemo(
-    () =>
-      filterFutureTransactions(
-        fixed.map((elem) => {
-          return {
-            ...elem,
-            date: makeDateFromDay(elem.dayOfMonth, selectedMonth + 1),
-          };
-        })
-      ),
-    [fixed, selectedMonth]
-  );
-
   const transactions = useMemo(
     () => [
-      ...fixed
-        .filter((elem) => elem.dayOfMonth <= new Date().getDate())
-        .map((elem) => {
-          return {
-            ...elem,
-            date: makeDateFromDay(elem.dayOfMonth, selectedMonth + 1),
-          };
-        }),
-      ...account
-        .map((elem) => {
-          return { ...elem, date: makeDateTimestamp(elem.effectiveDate) };
-        })
-        .filter((elem) => getMonth(elem.date) === selectedMonth + 1),
+      ...account.map((elem) => {
+        return { ...elem, date: makeDateTimestamp(elem.effectiveDate) };
+      }),
     ],
-    [fixed, account, selectedMonth]
+    [account]
   );
 
   const handleFilter = (value) => {
+    console.log("here");
+    console.log(value);
     switch (value) {
       case "all":
         setFilteredTransactions([...transactions]);
         setFilteredFutureTransactions([...futureTransactions]);
         break;
+
       default:
         setFilteredTransactions(
           [...transactions].filter((elem) => elem.type === value)
