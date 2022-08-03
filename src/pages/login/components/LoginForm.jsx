cb31930eba4c8da9e9bd81db944fa8b2e96dd4e7
ayelet <ayelet2708@gmail.com>
2022-08-03 22:07:19 +0300

    edit: login page

--- src/pages/login/components/LoginForm.jsx
+++ src/pages/login/components/LoginForm.jsx
@@ -4,51 +4,51 @@ import * as Yup from "yup";
 import { Flex } from "monday-ui-react-core";
 
 import {
-  Form,
-  FormTextField,
-  FormSubmitButton,
+	Form,
+	FormTextField,
+	FormSubmitButton,
 } from "../../../components/form";
 import { loginUser } from "../../../redux/users/actions/loginUser";
 
 const initialValues = {
-  email: "",
-  password: "",
+	email: "",
+	password: "",
 };
 
 const validationSchema = Yup.object().shape({
-  email: Yup.string().email().required("Required").label("Email"),
-  password: Yup.string().min(4).required("Required").label("Password"),
+	email: Yup.string().email().required("Required").label("Email"),
+	password: Yup.string().min(4).required("Required").label("Password"),
 });
 
 const LoginForm = () => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const isLoading = useSelector(({ usersState }) => usersState.isLoading);
+	const isLoading = useSelector(({ usersState }) => usersState.isLoading);
 
-  const onLoginUser = useCallback(
-    (values) => {
-      dispatch(loginUser(values));
-    },
-    [dispatch]
-  );
+	const onLoginUser = useCallback(
+		(values) => {
+			dispatch(loginUser(values));
+		},
+		[dispatch]
+	);
 
-  return (
-    <Form
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={onLoginUser}
-    >
-      <Flex gap={Flex.gaps.SMALL} direction={Flex.directions.COLUMN}>
-        <FormTextField field="email" placeholder="Email" autoFocus />
-        <FormTextField
-          field="password"
-          placeholder="Password"
-          type="password"
-        />
-        <FormSubmitButton label="Submit" isLoading={isLoading} />
-      </Flex>
-    </Form>
-  );
+	return (
+		<Form
+			initialValues={initialValues}
+			validationSchema={validationSchema}
+			onSubmit={onLoginUser}
+		>
+			<Flex gap={Flex.gaps.SMALL} direction={Flex.directions.COLUMN}>
+				<FormTextField field="email" placeholder="Email" autoFocus />
+				<FormTextField
+					field="password"
+					placeholder="Password"
+					type="password"
+				/>
+				<FormSubmitButton label="Login" isLoading={isLoading} />
+			</Flex>
+		</Form>
+	);
 };
 
 export default LoginForm;
