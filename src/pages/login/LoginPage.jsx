e3f268e2c66dcd814c6abb133b3a27a89b5403ff
itay <itayg43@gmail.com>
2022-08-04 00:35:58 +0300

    fix: bugs after merge

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink, useNavigate } from "react-router-dom";
-import { Heading } from "monday-ui-react-core";
 
 import { APP_ROUTES } from "../../helpers/constants";
 import { selectUser } from "../../redux/user/selectors";
@@ -10,8 +9,8 @@ import logo from "../../assets/up_logo.png";
 import styles from "./loginPage.module.css";
 
 const LoginPage = () => {
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const user = useSelector(selectUser);
 
@@ -21,18 +20,18 @@ const LoginPage = () => {
     }
   }, [dispatch, navigate, user]);
 
-	return (
-		<div className={styles.container}>
-			<img src={logo} alt="" className={styles.logo} />
-			
-			<div className={styles.formContainer}>
-				<LoginForm />
-			</div>
-			<div className={styles.linkContainer}>
-				<NavLink to={ROUTES.REGISTER}>Need to register?</NavLink>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <img src={logo} alt="" className={styles.logo} />
+
+      <div className={styles.formContainer}>
+        <LoginForm />
+      </div>
+      <div className={styles.linkContainer}>
+        <NavLink to={APP_ROUTES.REGISTER}>Need to register?</NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
