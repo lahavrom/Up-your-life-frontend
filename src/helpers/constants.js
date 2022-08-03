a0d9964ece89d98e215dcba6068b800cb877d752
Lahav Rom <lahavrom@gmail.com>
2022-08-03 16:10:14 +0300

    feat: small UI changes

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,68 +1,68 @@
 export const ERROR_MESSAGES = {
-	DEFAULT:
-		"Something went wrong, please try again in a few minutes or contact support",
+  DEFAULT:
+    "Something went wrong, please try again in a few minutes or contact support",
 };
 
 export const ABOUT_INFO = {
-	DESCRIPTION:
-		"Up Your Biz is app that will help save money and will calculate the amount of money the user can spend each month. From now on you can manage your income and expenses in the most easy and quick way!",
-	FEATURES: [
-		"Multipal house holders",
-		"Show expenses by categories",
-		"Show incomes by categories",
-	],
+  DESCRIPTION:
+    "Up Your Biz is an app that will help you save money by tracking your expenses and incomes",
+  FEATURES: [
+    "Multiple users per account",
+    "Show expenses by categories",
+    "Show incomes by categories",
+  ],
 };
 
 export const SUCCESS_MESSAGES = {
-	SUBMIT_TRANSACTION: "Transaction added successfully!",
-	EDIT_TRANSACTION: "Transaction editted successfully!",
-	DELETE_TRANSACTION: "Transaction deleted successfully!",
+  SUBMIT_TRANSACTION: "Transaction added successfully!",
+  EDIT_TRANSACTION: "Transaction editted successfully!",
+  DELETE_TRANSACTION: "Transaction deleted successfully!",
 };
 
 export const MONTHS = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December",
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 
 export const COLORS = [
-	"#76C893",
-	"#C60F7B",
-	"#168AAD",
-	"#d89d6a",
-	"#1A759F",
-	"#184E77",
-	"#FBB454",
+  "#76C893",
+  "#C60F7B",
+  "#168AAD",
+  "#d89d6a",
+  "#1A759F",
+  "#184E77",
+  "#FBB454",
 ];
 
 export const DAYS_OF_MONTH = [
-	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-	23, 24, 25, 26, 27, 28, 29, 30, 31,
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30, 31,
 ];
 
 export const CATEGORIES = {
-	CAR: "Car",
-	CONSTRUCTION: "Construction",
-	FOOD: "Food",
-	OTHER: "Other",
-	PAYCHECK: "Paycheck",
-	PETS: "Pets",
-	RENT: "Rent",
-	SCHOOL: "School",
-	SHOPPING: "Shopping",
-	VACATION: "Vacation",
+  CAR: "Car",
+  CONSTRUCTION: "Construction",
+  FOOD: "Food",
+  OTHER: "Other",
+  PAYCHECK: "Paycheck",
+  PETS: "Pets",
+  RENT: "Rent",
+  SCHOOL: "School",
+  SHOPPING: "Shopping",
+  VACATION: "Vacation",
 };
 
 export const TRANSACTIONS_TYPES = {
-	INCOME: "Income",
-	EXPENSE: "Expense",
+  INCOME: "Income",
+  EXPENSE: "Expense",
 };
