82956a5037890567a299952904bd2f45b64c9ec6
itay <itayg43@gmail.com>
2022-08-04 17:26:45 +0300

    edit: create add user to account

--- src/redux/account/reducer.js
+++ src/redux/account/reducer.js
@@ -3,6 +3,7 @@ import ACTION_TYPES from "./actions/constants/actionTypes";
 const initialState = {
   isLoading: false,
   isSuccess: false,
+  successMessage: "",
   isError: false,
   errorMessage: "",
   users: [],
@@ -10,11 +11,13 @@ const initialState = {
 
 const accountReducer = (state = initialState, { type, payload }) => {
   switch (type) {
-    case ACTION_TYPES.FETCH_ACCOUNT_USERS: {
+    case ACTION_TYPES.FETCH_ACCOUNT_USERS:
+    case ACTION_TYPES.ADD_USER_TO_ACCOUNT: {
       return {
         ...state,
         isLoading: true,
         isSuccess: false,
+        successMessage: "",
         isError: false,
         errorMessage: "",
       };
@@ -32,12 +35,26 @@ const accountReducer = (state = initialState, { type, payload }) => {
       };
     }
 
-    case ACTION_TYPES.FETCH_ACCOUNT_USERS_FAIL: {
+    case ACTION_TYPES.ADD_USER_TO_ACCOUNT_SUCCESS: {
+      const { successMessage } = payload;
+      return {
+        ...state,
+        isLoading: false,
+        isSuccess: true,
+        successMessage,
+        isError: false,
+        errorMessage: "",
+      };
+    }
+
+    case ACTION_TYPES.FETCH_ACCOUNT_USERS_FAIL:
+    case ACTION_TYPES.ADD_USER_TO_ACCOUNT_FAIL: {
       const { errorMessage } = payload;
       return {
         ...state,
         isLoading: false,
         isSuccess: false,
+        successMessage: "",
         isError: true,
         errorMessage,
       };
