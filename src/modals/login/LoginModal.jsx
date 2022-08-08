a55d43795bf2e514de07b54ff1063ad00f281612
Lahav Rom <lahavrom@gmail.com>
2022-08-08 14:04:45 +0300

    headers in login & register modals

--- src/modals/login/LoginModal.jsx
+++ src/modals/login/LoginModal.jsx
@@ -1,12 +1,14 @@
 import AppModal from "../../components/modal/AppModal";
 import LoginForm from "./components/LoginForm";
 import styles from "./loginModal.module.css";
+import { Heading } from "monday-ui-react-core";
 
 const LoginModal = ({ isOpen, onClose }) => {
   return (
     <AppModal isOpen={isOpen} onClose={onClose}>
       <div className={styles.container}>
         <div className={styles.formContainer}>
+          <Heading value="Login" className={styles.header} />
           <LoginForm />
         </div>
       </div>
--- src/modals/register/RegisterModal.jsx
+++ src/modals/register/RegisterModal.jsx
@@ -1,11 +1,13 @@
 import AppModal from "../../components/modal/AppModal";
 import RegisterForm from "./components/RegisterForm";
 import styles from "./registerModal.module.css";
+import { Heading } from "monday-ui-react-core";
 
 const RegisterModal = ({ isOpen, onClose }) => {
   return (
     <AppModal isOpen={isOpen} onClose={onClose}>
       <div className={styles.container}>
+        <Heading value="Register" className={styles.header} />
         <div className={styles.formContainer}>
           <RegisterForm />
         </div>
