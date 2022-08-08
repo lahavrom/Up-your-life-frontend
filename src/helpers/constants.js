a9fee73c1e403342a43c8af0de1e4270099da3f5
Lahav Rom <lahavrom@gmail.com>
2022-08-08 15:11:19 +0300

    fix

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -15,8 +15,8 @@ export const ABOUT_INFO = {
 };
 
 export const APP_ROUTES = {
-  LANDING_PAGE: "/",
-  UP_YOUR_BIZ: "/home",
+  LANDING_PAGE: "/home",
+  UP_YOUR_BIZ: "/",
 };
 
 export const SUCCESS_MESSAGES = {
